cfdb98f5fb023839f34cc50d79efcee0062f0fce
Thierry Charbonnel <t@ap.cx>
2017-09-03 17:01:38 -0400

    Deploy gh-pages

--- _next/531a94cf-7d4a-4c45-93c1-4bf4a547ad0b/page/index.js
+++ _next/531a94cf-7d4a-4c45-93c1-4bf4a547ad0b/page/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([2],[],[357]);
+            var comp = module.exports=webpackJsonp([2],[],[372]);
             return { page: comp.default }
           })
         
