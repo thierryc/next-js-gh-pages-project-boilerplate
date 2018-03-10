5875c856c811e26d50981a6a9aac8746dc5ad9a8
Thierry Charbonnel <thierryc@users.noreply.github.com>
2018-03-10 13:03:31 -0500

    Deploy gh-pages

--- _next/c7bf9944-ea3e-4676-8777-7f8ca5d0d415/page/index.js
+++ _next/c7bf9944-ea3e-4676-8777-7f8ca5d0d415/page/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([2],[],[373]);
+            var comp = module.exports=webpackJsonp([2],[],[364]);
             return { page: comp.default }
           })
         
